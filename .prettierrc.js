ed51502f0a07df9951a5f3cf3500ed50a7b7b23e
vijayhardaha <vijayhardaha@gmail.com>
2023-10-10 18:46:17 +0530

    chore(remove): remove prettier config file

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-// Import the default Prettier config file and expose it in the project root.
-// Useful for editor integrations.
-
-// Note: The `module.exports` statement is not necessary in Node.js v18 and later,
-// as the default export is automatically exposed. However, we are including it
-// for compatibility with older versions of Node.js.
-
-module.exports = require('@wordpress/prettier-config');

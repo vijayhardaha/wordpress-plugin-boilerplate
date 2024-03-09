eb564a57fcb9167048fb64537152274fc0e20662
Vijay Hardaha <vijayhardaha@gmail.com>
2024-03-09 16:58:34 +0530

    chore: write license name in valid SPDX expression

--- custom-plugin.php
+++ custom-plugin.php
@@ -6,7 +6,7 @@
  * Version: 1.0.0
  * Author: Vijay Hardaha
  * Author URI: https://twitter.com/vijayhardaha/
- * License: GPLv2+
+ * License: GPL-2.0-or-later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: custom-plugin
  * Domain Path: /languages/

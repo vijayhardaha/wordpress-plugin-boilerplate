58bcfd7252144ea8e79f54881236295ddccb4d3f
Vijay Hardaha <vijayhardaha@proton.me>
2026-05-20 08:54:02 +0530

    ci: add commitlint configuration

---
+++ commitlint.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * =======================================================================
+ * Commitlint Configuration
+ * =======================================================================
+ * Purpose: Enforce conventional commits and project commit style.
+ * Docs:    https://commitlint.js.org/#/
+ * =======================================================================
+ */
+
+import config from '@vijayhardaha/dev-config/commitlint';
+
+export default config;

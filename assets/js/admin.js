7657cc8b83d07eee589f32e8fdc1b4f16f85160b
Vijay Hardaha <vijayhardaha@proton.me>
2026-05-20 08:50:02 +0530

    feat: refactor admin JS to modern IIFE syntax

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line no-unused-vars
-( function( $ ) {
-	// Write your jQuery or JS code here.
-	console.log( 'Custom Plugin backend js working...' );
-}( jQuery ) );
+(function ($) {
+  // Write your jQuery or JS code here.
+  console.log('Custom Plugin backend js working...');
+})(jQuery);

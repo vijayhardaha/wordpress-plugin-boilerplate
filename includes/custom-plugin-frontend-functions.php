3e0e9890bf4cf866d8555de80e36ff09f90edc28
Vijay Hardaha <vijayhardaha@proton.me>
2026-05-20 08:50:09 +0530

    refactor: add GPL header and type declarations

--- includes/custom-plugin-frontend-functions.php
+++ includes/custom-plugin-frontend-functions.php
@@ -2,17 +2,37 @@
 /**
  * Frontend-specific functions for the Custom_Plugin.
  *
+ * @version 1.0.0
  * @package Custom_Plugin
+ * @author Vijay Hardaha <https://pph.me/vijayhardaha/>
+ *
+ * Copyright (C) 2026 Vijay Hardaha
+ *
+ * Licensed under the GNU General Public License v2 or later.
+ * You may redistribute and/or modify this software under
+ * the terms of the GPL as published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Attribution: This code is part of the Custom Plugin plugin, developed by
+ *
+ * ██╗   ██╗██╗     ██╗ █████╗ ██╗   ██╗    ██╗  ██╗ █████╗ ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗
+ * ██║   ██║██║     ██║██╔══██╗╚██╗ ██╔╝    ██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║  ██║██╔══██╗
+ * ██║   ██║██║     ██║███████║ ╚████╔╝     ███████║███████║██████╔╝██║  ██║███████║███████║███████║
+ * ╚██╗ ██╔╝██║██   ██║██╔══██║  ╚██╔╝      ██╔══██║██╔══██║██╔══██╗██║  ██║██╔══██║██╔══██║██╔══██║
+ *  ╚████╔╝ ██║╚█████╔╝██║  ██║   ██║       ██║  ██║██║  ██║██║  ██║██████╔╝██║  ██║██║  ██║██║  ██║
+ *   ╚═══╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
  */
 
-// Prevent direct access to this file.
-defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+// Exit if undefined.
+defined( 'ABSPATH' ) || exit;
 
 /**
  * A description of the custom_plugin_frontend_function.
  *
  * @since 1.0.0
  */
-function custom_plugin_frontend_function() {
+function custom_plugin_frontend_function(): void {
 	// Write your function code here.
 }

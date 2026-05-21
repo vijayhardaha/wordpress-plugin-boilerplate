920fb3c724f7b80d9fa5d100efad743644b3917b
Vijay Hardaha <vijayhardaha@proton.me>
2026-05-21 06:34:47 +0530

    chore: update plugin attribution text

--- custom-plugin.php
+++ custom-plugin.php
@@ -25,7 +25,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * Attribution: This code is part of the ZT User Tags plugin, developed by
+ * Attribution: This code is part of the Custom Plugin plugin, developed by
  *
  * ██╗   ██╗██╗     ██╗ █████╗ ██╗   ██╗    ██╗  ██╗ █████╗ ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗
  * ██║   ██║██║     ██║██╔══██╗╚██╗ ██╔╝    ██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║  ██║██╔══██╗
